d7971f80924bc8168bf0e3ae84e31c11198217b1
Eugene <eugenemisiko438@gmail.com>
2026-02-27 01:59:23 +0300

    creating a form for the creating of order

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authslice"
+import itemReducer from "./slices/authslice"
 
 export const Store = configureStore({
   reducer: {
     auth: authReducer,
+    items: itemReducer,
   },
 });
 
