15effa8d33d98201e2481e838e8e370e2b855c30
Eugene <eugenemisiko438@gmail.com>
2026-03-03 10:02:23 +0300

    updating the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,114 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./components/login"
-import Register from "./components/registration"
-import Home from "./components/Home"
-import ItemOrderForm from "./components/ItemOrderForm"
-import ItemOrderList from "./components/ItemOrderList"
-import Layout from "./components/Layout"
-import Profile from "./components/Profile"
-import UserList from "./components/UserList"
-import AdminOrders from "./components/AdminOrders"
-import ClientOrder from "./components/ClientOrder"
-import ClientOrders from "./components/ClientHistory"
-import Products from "./components/Products"
-import ProductDetail from "./components/ProductDetail"
-import PrinterDashboard from "./components/PrinterDashboard"
-import { useSelector } from "react-redux"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import Login from "./components/login";
+import Register from "./components/registration";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import UserList from "./components/UserList";
+import ClientOrder from "./components/ClientOrder";
+import ClientOrders from "./components/ClientHistory";
+import Products from "./components/Products";
+import ProductDetail from "./components/ProductDetail";
+import PrinterDashboard from "./components/PrinterDashboard";
+import DesignerDashboard from "./components/DesignerDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   const { user } = useSelector((state) => state.auth);
+
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/list" element={<ItemOrderList />} />
-          <Route path="/create" element={<ItemOrderForm />} />
-          <Route path="/admin-orders" element={<AdminOrders />}/>
-          <Route path="/client-orders/:product?Id?" element={<ClientOrder/>}/>
-          <Route path="/create-order/:productId?" element={<ClientOrder />} />
-          <Route path="/history" element={<ClientOrders/>}/>
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/printer" element={user?.role ==="printer" ? (<PrinterDashboard />): (<Navigate to="/"/>)}/>
+
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute allowedRoles={["client"]}>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/products"
+            element={
+              <ProtectedRoute allowedRoles={["client"]}>
+                <Products />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/products/:id"
+            element={
+              <ProtectedRoute allowedRoles={["client"]}>
+                <ProductDetail />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/create-order/:productId?"
+            element={
+              <ProtectedRoute allowedRoles={["client"]}>
+                <ClientOrder />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/history"
+            element={
+              <ProtectedRoute allowedRoles={["client"]}>
+                <ClientOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <UserList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/designer"
+            element={
+              <ProtectedRoute allowedRoles={["designer"]}>
+                <DesignerDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* PRINTER ROUTES */}
+          <Route
+            path="/printer"
+            element={
+              <ProtectedRoute allowedRoles={["printer"]}>
+                <PrinterDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* COMMON */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute allowedRoles={["client", "admin", "designer", "printer"]}>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
         </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
