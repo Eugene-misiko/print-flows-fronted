7a76288ff1fd6521dbe48585928820791c07a0b7
Eugene <eugenemisiko438@gmail.com>
2026-03-16 08:33:32 +0300

    update the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Login from "./components/login";
-import Register from "./components/registration";
+
+import Login from "./components/Login";
+import Register from "./components/Registration";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
@@ -11,11 +12,11 @@ import ClientOrders from "./components/ClientHistory";
 import Products from "./components/Products";
 import ProductDetail from "./components/ProductDetail";
 import PrinterDashboard from "./components/PrinterDashboard";
-import DesignerDashboard from "./components/DesignerDashboard.jsx";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Landing from "./components/Landing";
+import DesignerDashboard from "./components/DesignerDashboard";
 import InvoicePage from "./components/InvoicePage";
 import PaymentPage from "./components/PaymentPage";
+import Landing from "./components/Landing";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const { user } = useSelector((state) => state.auth);
@@ -23,22 +24,22 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* PUBLIC ROUTES */}
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Landing />} />
-        <Route path="/InvoicePage/:id" element={<InvoicePage />} />
         <Route path="/invoice/:id" element={<InvoicePage />} />
-        <Route path="/payment" element={<PaymentPage />} />
         <Route path="/payment/:id" element={<PaymentPage />} />
-        
+        {/* PROTECTED ROUTES */}
         <Route element={<Layout />}>
+          {/* CLIENT DASHBOARD */}
           <Route
             path="/authen"
             element={
               <ProtectedRoute allowedRoles={["client"]}>
                 <Home />
               </ProtectedRoute>}/>
-
+          {/* PRODUCTS */}
           <Route
             path="/products"
             element={
@@ -51,46 +52,48 @@ function App() {
               <ProtectedRoute allowedRoles={["client"]}>
                 <ProductDetail />
               </ProtectedRoute>}/>
-
+          {/* CREATE ORDER */}
           <Route
             path="/create-order/:productId?"
             element={
               <ProtectedRoute allowedRoles={["client"]}>
                 <ClientOrder />
               </ProtectedRoute>}/>
+          {/* ORDER HISTORY */}
           <Route
             path="/history"
             element={
               <ProtectedRoute allowedRoles={["client"]}>
                 <ClientOrders />
               </ProtectedRoute>}/>
+          {/* ADMIN */}
           <Route
             path="/users"
             element={
               <ProtectedRoute allowedRoles={["admin"]}>
                 <UserList />
               </ProtectedRoute>}/>
+          {/* DESIGNER DASHBOARD */}
           <Route
             path="/designer"
             element={
               <ProtectedRoute allowedRoles={["designer"]}>
                 <DesignerDashboard />
               </ProtectedRoute>}/>
-          {/* PRINTER ROUTES */}
+          {/* PRINTER DASHBOARD */}
           <Route
             path="/printer"
             element={
               <ProtectedRoute allowedRoles={["printer"]}>
                 <PrinterDashboard />
-              </ProtectedRoute> }/>
-
-          {/* COMMON */}
+              </ProtectedRoute>}/>
+          {/* COMMON PROFILE */}
           <Route
             path="/profile"
             element={
-              <ProtectedRoute allowedRoles={["client", "admin", "designer", "printer"]}>
+              <ProtectedRoute allowedRoles={["client","admin","designer","printer"]}>
                 <Profile />
-              </ProtectedRoute> }/>
+              </ProtectedRoute>}/>
         </Route>
       </Routes>
     </Router>
