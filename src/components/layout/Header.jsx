f92b9021f0368098a868c921783b19f2d2783342
Eugene <eugenemisiko438@gmail.com>
2026-03-22 13:10:29 +0300

    create the Header component in the layout folder

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Bell, MessageSquare, Search, Menu } from "lucide-react";
+import { useDispatch } from "react-redux";
