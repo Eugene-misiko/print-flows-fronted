14160af0d7dbd624f3be265a99f4b6da60df8364
Eugene <eugenemisiko438@gmail.com>
2026-03-31 01:18:33 +0300

    update the styling for the light and dark mode

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -8,11 +8,11 @@ const Layout = () => {
   const { user } = useSelector((state) => state.auth);
 
   return (
-    <div className="flex dark:text-black min-h-screen dark:from-gray-950 dark:via-gray-900 dark:to-gray-800 bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700">
+    <div className="flex min-h-screen bg-gray-50 dark:bg-gradient-to-r dark:from-gray-950 dark:via-gray-900 dark:to-gray-800 text-gray-900 dark:text-white transition-colors duration-200">
       <Sidebar user={user} />
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="flex-1 p-6 overflow-auto">
+        <main className="flex-1 p-4 md:p-6 overflow-auto">
           <Outlet />
         </main>
       </div>
