cbd23809eeff1d834b79d6bbb71fb10ad17d489b
Eugene <eugenemisiko438@gmail.com>
2026-04-07 11:26:52 +0300

    update the axios interceptors

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import { jwtDecode } from "jwt-decode";
 // BASE AXIOS INSTANCE
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 // TOKEN HELPERS
